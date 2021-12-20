782a6711ad805cb08dd0d271cf3b5beb2edfff30
YanaOsk <yanaoskin35@gmail.com>
2021-12-20 14:26:21 +0200

    add a test directory

---
+++ Test/test_GraphAlgo.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+
+class TestGraphAlgo(TestCase):
+    def test_get_graph(self):
+        self.fail()
+
+    def test_load_from_json(self):
+        self.fail()
+
+    def test_save_to_json(self):
+        self.fail()
+
+    def test_shortest_path(self):
+        self.fail()
+
+    def test_dijkstra(self):
+        self.fail()
+
+    def test_tsp(self):
+        self.fail()
+
+    def test_center_point(self):
+        self.fail()
+
+    def test_floyd_warshall(self):
+        self.fail()
+
+    def test_plot_graph(self):
+        self.fail()
